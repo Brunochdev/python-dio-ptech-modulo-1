0f9de808f978ad029a98126570d55e4b6a780267
Bruno Chagas <139476631+Brunochdev@users.noreply.github.com>
2023-07-18 15:17:47 -0300

    tipos-python
    Exemplos de como funcionam os tipos no python quando impressos em tela.

---
+++ tipos-python.py
@@ -0,0 +1,4 @@
+print(21 + 14) #números inteiros (int)
+print(1.5 + 5 + 3.8) #números inteiros (int) mais ponto flutuante (float)
+print(True) #tipo booleano, o parâmetro vai ser verdadeiro ou falso (bool)
+print('Bruno') #tipo string deve ser colocado entre aspas
