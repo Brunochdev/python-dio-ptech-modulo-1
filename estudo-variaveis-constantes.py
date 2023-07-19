950fb552b9ef74f011e53e11de8c68223a26e508
Bruno Chagas <139476631+Brunochdev@users.noreply.github.com>
2023-07-19 17:59:17 -0300

    Estudo de variáveis e constantes
    Entendendo como utilizar variáveis e constantes no python

---
+++ estudo-variaveis-constantes.py
@@ -0,0 +1,14 @@
+nome = 'Bruno' #caso haja uma nova chamada de variável com o mesmo nome, ficará valendo a última que foi chamada
+               #ex: caso o nome da variável nome seja mudado para Pedro na linha 4, a frase terá Pedro e não Bruno.
+idade = 35
+
+nome, idade = 'Bruno', 35 #lembrar que é possível chamar as variáveis todas em uma linha só
+
+
+
+
+ANO_DE_NASCIMENTO = 1987 #caso haja uma nova chamada de variável com o mesmo nome, ficará valendo a última que foi chamada
+                         #ex: caso o nome da variável nome seja mudado para 1994 na linha 13, a frase terá 1994 e não 1987.
+
+
+print('Seu nome é', nome  ,'e sua idade', idade, 'já que seu ano de nascimento foi', ANO_DE_NASCIMENTO)
