efa8613665addc335b0e76f7bfffff1e9bea17f8
Bruno Chagas <139476631+Brunochdev@users.noreply.github.com>
2023-07-20 19:27:42 -0300

    Operadores de comparação
    Estudo de operadores (==, >=, <, etc...)

---
+++ operadores_comparativos.py
@@ -0,0 +1,9 @@
+##Operadores de Comparação
+
+saldo = 450
+saque = 200
+print(saldo == saque)  #operador '==' quer dizer igual
+print(saldo != saque)  #operador '!=' quer dizer diferente
+print(saldo > saque)  #operador '>' quer dizer maior que
+print(saldo < saque)  #operador '<' quer dizer menor que
+print(saldo >= saque)  #operador '>=' quer dizer maior ou igual
